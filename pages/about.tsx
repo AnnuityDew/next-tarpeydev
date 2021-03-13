835352a12f8b50a1ac0fcb525fc6dca727450608
AnnuityDew <annuitydew@gmail.com>
2021-03-12 21:02:37 -0500

    "Coming soon" stuff

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import Page from "../components/Page"
+import { ExternalLink } from "../components/ExternalLink"
 
 export default function About() {
   return (
@@ -9,8 +10,16 @@ export default function About() {
       subheading="Where did all of the apps go?"
     >
       <p>
-        I'm rewriting the site with Next.js! This site will slowly expand to
+        I'm rewriting the site in React with Next.js! This site will slowly expand to
         include everything that was on the old site.
+      </p>
+      <p>
+        For now, to view the other apps that haven't moved over here yet,
+        visit <ExternalLink href="https://timecapsule.tarpey.dev">timecapsule.tarpey.dev</ExternalLink>.
+        (Yes, that means there's a timecapsule INSIDE the timecapsule now...)
+      </p>
+      <p>
+        For less about apps and more about me, visit <ExternalLink href="https://miketarpey.com">miketarpey.com</ExternalLink>.
       </p>
     </Page>
   )
