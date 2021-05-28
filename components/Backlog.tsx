d34ed116bc63d1d5987055f0a50295e652cea7c3
AnnuityDew <annuitydew@gmail.com>
2021-05-27 22:15:20 -0400

    Working on Backlog admin

--- components/Backlog.tsx
+++ components/Backlog.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { StyledButton } from "./Buttons"
 
 interface BacklogGameData {
   game_title: string
@@ -31,6 +32,15 @@ export const GameDiv = styled.div`
   text-align: center;
   margin: 20px 0;
   padding: 10px;
+  label {
+    text-align: right;
+    margin-right: 15px;
+    width: 25%;
+  }
+  form {
+    max-width: 1000px;
+  }
+  form label
 `
 
 export function BacklogGame({ gameData }: BacklogGameProps) {
@@ -38,14 +48,77 @@ export function BacklogGame({ gameData }: BacklogGameProps) {
     <GameDiv>
       <h4>{gameData.game_title}</h4>
       <h5>{gameData.sub_title}</h5>
-      {gameData.game_system} | {gameData.genre} | {gameData.game_status}<br/>
-      Now Playing: {gameData.now_playing.toString()} | DLC: {gameData.dlc.toString()}<br/>
-      Playtime: {gameData.game_hours} hours, {gameData.game_minutes} minutes<br/>
-      Added {gameData.add_date || ": N/A"}<br/>
-      Started {gameData.start_date || ": N/A"}<br/>
-      Beaten {gameData.beat_date || ": N/A"}<br/>
-      Completed {gameData.complete_date || ": N/A"}<br/>
-      <h6>{gameData.game_notes}</h6>      
+      {gameData.game_system} | {gameData.genre} | {gameData.game_status}<br />
+      Now Playing: {gameData.now_playing.toString()} | DLC: {gameData.dlc.toString()}<br />
+      Playtime: {gameData.game_hours} hours, {gameData.game_minutes} minutes<br />
+      Added {gameData.add_date || ": N/A"}<br />
+      Started {gameData.start_date || ": N/A"}<br />
+      Beaten {gameData.beat_date || ": N/A"}<br />
+      Completed {gameData.complete_date || ": N/A"}<br />
+      <h6>{gameData.game_notes}</h6>
+    </GameDiv>
+  )
+}
+
+const StyledBacklogForm = styled.form`
+  display: flex;
+`
+
+const BacklogBasics = styled.fieldset`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+`
+
+export function BacklogGameForm() {
+  return (
+    <GameDiv>
+      <StyledBacklogForm>
+        <fieldset>
+          <label htmlFor="game_title">Game Title</label>
+          <input id="game_title" name="game_title" type="text" autoComplete="game_title" required />
+          <label htmlFor="sub_title">Sub Title</label>
+          <input id="sub_title" name="sub_title" type="text" autoComplete="sub_title" required />
+          <label htmlFor="game_system">System</label>
+          <input id="game_system" name="game_system" type="text" autoComplete="game_system" required />
+          <label htmlFor="genre">Genre</label>
+          <input id="genre" name="genre" type="text" autoComplete="genre" required />
+          <label htmlFor="game_status">Status</label>
+          <select id="game_status" name="game_status" required>
+            <option value="Not Started">Not Started</option>
+            <option value="Started">Started</option>
+            <option value="Beaten">Finished (Any%)</option>
+            <option value="Completed">Finished (100%)</option>
+            <option value="Mastered">Mastered</option>
+            <option value="Infinite">Infinite</option>
+            <option value="Wish List">Wish List</option>
+          </select>
+          <label htmlFor="now_playing">Now Playing</label>
+          <select id="now_playing" name="now_playing" required>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          <label htmlFor="dlc">DLC</label>
+          <select id="dlc" name="dlc" required>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          <label htmlFor="game_hours">Hours Played</label>
+          <input id="game_hours" name="game_hours" type="number" autoComplete="game_hours" min="0" required />
+          <label htmlFor="game_minutes">Minutes Played</label>
+          <input id="game_minutes" name="game_minutes" type="number" autoComplete="game_minutes" min="0" max="59" required />
+          <label htmlFor="add_date">Date Acquired</label>
+          <input id="add_date" name="add_date" type="date" autoComplete="add_date" required />
+          <label htmlFor="start_date">Date Started</label>
+          <input id="start_date" name="start_date" type="date" autoComplete="start_date" required />
+          <label htmlFor="beat_date">Date Beaten</label>
+          <input id="beat_date" name="beat_date" type="date" autoComplete="beat_date" required />
+          <label htmlFor="complete_date">Date Completed</label>
+          <input id="complete_date" name="complete_date" type="date" autoComplete="complete_date" required />
+          <label htmlFor="game_notes">Game Notes</label>
+          <textarea id="game_notes" name="game_notes">Test</textarea>
+        </fieldset>
+        <button type="submit">Add Game</button>
+      </StyledBacklogForm>
     </GameDiv>
   )
 }
---
+++ pages/backlog-admin.tsx
@@ -0,0 +1,109 @@
+// https://github.com/nextauthjs/next-auth-example/blob/main/pages/protected.js
+import { useState } from 'react'
+import Page from "../components/Page"
+import { useSession, getSession } from 'next-auth/client'
+import { StyledButton } from "../components/Buttons"
+import {
+  BacklogGame, BacklogGameForm
+} from "../components/Backlog"
+
+// This gets called only on build
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+  let api = ""
+  if (process.env.API_ENV === "test") {
+    api = "http://127.0.0.1:8000"
+  } else if (process.env.API_ENV === "dev") {
+    api = "https://dev.tarpey.dev"
+  } else {
+    api = "https://api.tarpey.dev"
+  }
+
+  // Pass data to the page via props
+  return { props: { apiUrl: api, session: await getSession(context) } }
+}
+
+export default function BacklogAdmin ({ apiUrl }) {
+  const [ session, loading ] = useSession()
+  const [ backlog, showBacklog ] = useState({visible: false, loading: false, data: ""})
+  const [ form, showForm ] = useState({visible: false, data: ""})
+  const [ filters, changeFilters ] = useState({dlcFilter: "", nowPlayingFilter: "", gameStatusFilter: ""})
+  const loadingText = "Loading..."
+  const subloadingText = "(takes a couple seconds!)"
+
+  if (typeof window !== 'undefined' && loading) return null
+
+  // If no session exists, display access denied message
+  if (!session) { return  <Page><h1>Access denied! Please login first.</h1></Page> }
+
+  async function backlogRequested(queryFilter) {
+    showBacklog(state => ({ ...state, loading: true }));
+    await Promise.all([
+      fetch(apiUrl + `/haveyouseenx/annuitydew/search?${queryFilter}`, { method: "GET", })
+        .then(response => { return response.json() })
+        .then(jsonData => { return JSON.stringify(jsonData) })
+        .then(jsonString => { showBacklog({ visible: true, loading: false, data: jsonString }) })
+    ]);
+  }
+
+  return (
+    <Page
+      titleTwo=" - Dew's Backlog"
+      description="A web app for viewing games, movies, and shows in my backlog."
+      url="https://tarpey.dev/backlog"
+      urlImage="https://tarpey.dev/backlog.png"
+      heading="Dew's Backlog"
+      subheading=""
+    >
+      <div>
+        <h4>Use the buttons below to look through my backlog of games.</h4>
+        <StyledButton
+          gridButton={false}
+          kind="medium"
+          label={backlog["loading"] ? loadingText : "Add a game"}
+          sublabel={backlog["loading"] ? subloadingText : ""}
+          click={() => showForm({visible: true, data: ""})}
+          disabled={backlog["loading"]}
+        />
+        <StyledButton
+          gridButton={false}
+          kind="blue"
+          label={backlog["loading"] ? loadingText : "Now Playing"}
+          sublabel={backlog["loading"] ? subloadingText : ""}
+          click={() => backlogRequested("now_playing=true")}
+          disabled={backlog["loading"]}
+        />
+        <StyledButton
+          gridButton={false}
+          kind="blue"
+          label={backlog["loading"] ? loadingText : "Not Started"}
+          sublabel={backlog["loading"] ? subloadingText : ""}
+          click={() => backlogRequested("game_status=Not%20Started")}
+          disabled={backlog["loading"]}
+        />
+        <StyledButton
+          gridButton={false}
+          kind="blue"
+          label={backlog["loading"] ? loadingText : "Started"}
+          sublabel={backlog["loading"] ? subloadingText : ""}
+          click={() => backlogRequested("game_status=Started")}
+          disabled={backlog["loading"]}
+        />
+      </div>
+      <div>
+        {form["visible"] ? <BacklogGameForm /> : null}
+      </div>
+      <div>
+        {backlog["visible"] &&
+          JSON.parse(backlog["data"])
+            .map((game, index) => (
+              <BacklogGame
+                key={index + 1}
+                gameData={game}
+              />
+            ))
+        }
+      </div>
+    </Page>
+  )
+}
